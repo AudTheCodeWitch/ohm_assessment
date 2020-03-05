c24f9cf7b6c5087f532d76f46511311b7961cabc
Audrea Cook <audreacwork@gmail.com>
2020-03-05 14:57:26 -0700

    Create dummy data for newest users

--- pages/community.py
+++ pages/community.py
@@ -10,6 +10,31 @@ def community():
 
     login_user(User.query.get(1))
 
+    # I haven't figured out how connect to MySQL, so I'm using dummy data for the time being
+    # topfive = conn.execute("SELECT display_name, tier, point_balance FROM user ORDER BY create_time DESC LIMIT 2;")
+    newest_users = [
+        {"display_name": "Justin Bieber",
+         "tier": "Silver",
+         "point_balance": 0
+         },
+        {"display_name": "Elvis Presley",
+         "tier": "Carbon",
+         "point_balance": 0
+         },
+        {"display_name": "Chuck Norris",
+         "tier": "Carbon",
+         "point_balance": 5000
+         },
+        {"display_name": "Rachel Carson",
+         "tier": "Platinum",
+         "point_balance": 12000
+         },
+        {"display_name": "Jane Goodall",
+         "tier": "Gold",
+         "point_balance": 8000
+         }
+    ]
+
     args = {
             'gift_card_eligible': True,
             'cashout_ok': True,
