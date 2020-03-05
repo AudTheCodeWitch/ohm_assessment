0cc545192ad1dd442135eaa56612cee45eec5f70
Audrea Cook <audreacwork@gmail.com>
2020-03-05 15:44:27 -0700

    Replace dummy data with class method - call User.get_five_newest_users() to get data for table

--- pages/community.py
+++ pages/community.py
@@ -10,30 +10,8 @@ def community():
 
     login_user(User.query.get(1))
 
-    # I haven't figured out how connect to MySQL, so I'm using dummy data for the time being
-    # topfive = conn.execute("SELECT display_name, tier, point_balance FROM user ORDER BY create_time DESC LIMIT 2;")
-    newest_users = [
-        {"display_name": "Justin Bieber",
-         "tier": "Silver",
-         "point_balance": 0
-         },
-        {"display_name": "Elvis Presley",
-         "tier": "Carbon",
-         "point_balance": 0
-         },
-        {"display_name": "Chuck Norris",
-         "tier": "Carbon",
-         "point_balance": 5000
-         },
-        {"display_name": "Rachel Carson",
-         "tier": "Platinum",
-         "point_balance": 12000
-         },
-        {"display_name": "Jane Goodall",
-         "tier": "Gold",
-         "point_balance": 8000
-         }
-    ]
+    newest_users = User.get_five_newest_users()
+
 
     args = {
             'gift_card_eligible': True,
